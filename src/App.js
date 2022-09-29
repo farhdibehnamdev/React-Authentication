d27199b541afb7cf56d8ee173a72e5f67bdfadea
farhdibehnamdev <97886945+farhdibehnamdev@users.noreply.github.com>
2022-09-29 23:25:41 +0330

    💄 Added ThemeProvider and customization theme.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,37 @@
-import "./App.css";
-import Routes from "./routes/Routes";
+import { ThemeProvider } from "@emotion/react";
+import { createTheme, CssBaseline, Box } from "@mui/material";
+import AppRoutes from "./routes/Routes";
+const theme = createTheme({
+  palette: {
+    background: {
+      default: "#F5F8FA",
+    },
+  },
+  components: {
+    MuiCard: {
+      styleOverrides: {
+        root: {
+          borderRadius: "20px",
+        },
+      },
+    },
+  },
+});
 function App() {
   return (
-    <>
-      <Routes />
-    </>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          height: "100vh",
+          justifyContent: "center",
+        }}
+      >
+        <AppRoutes />
+      </Box>
+    </ThemeProvider>
   );
 }
 
