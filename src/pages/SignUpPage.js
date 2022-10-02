5e5695f4659d8268ee8d33953c18bc74752a2044
farhdibehnamdev <97886945+farhdibehnamdev@users.noreply.github.com>
2022-10-02 22:09:11 +0330

    chore:Added signupPage.

---
+++ src/pages/SignUpPage.js
@@ -0,0 +1,12 @@
+import { Grid } from "@mui/material";
+import SignUp from "../components/signin/SignUp";
+
+const SignUpPage = function () {
+  return (
+    <Grid container>
+      <SignUp />
+    </Grid>
+  );
+};
+
+export default SignUpPage;
