22259854b8ff5c7f108fabd9da92adf04fcd07d0
farhdibehnamdev <97886945+farhdibehnamdev@users.noreply.github.com>
2022-10-03 19:15:52 +0330

    🚚 Renamed auth folder to privateRoute

--- src/routes/auth/PrivateRoute.js
+++
@@ -1,8 +0,0 @@
-import { Navigate, Route, Outlet } from "react-router-dom";
-const PrivateRoute = function () {
-  const auth = "null";
-
-  return auth ? <Outlet /> : <Navigate to="/signin" />;
-};
-
-export default PrivateRoute;
