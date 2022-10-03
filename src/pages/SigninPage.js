f43e21f65c6b4da0430316676c671665833250d3
farhdibehnamdev <97886945+farhdibehnamdev@users.noreply.github.com>
2022-10-03 19:18:19 +0330

    🚚 Added SignInPage in pages folder.

---
+++ src/pages/SigninPage.js
@@ -0,0 +1,12 @@
+import { Grid } from "@mui/material";
+import SignIn from "../components/signin/SignIn";
+
+const SignInPage = function () {
+  return (
+    <Grid container>
+      <SignIn />
+    </Grid>
+  );
+};
+
+export default SignInPage;
