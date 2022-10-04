ad9fb43e4daefce11a089ea83cc483cfa67c029e
farhdibehnamdev <97886945+farhdibehnamdev@users.noreply.github.com>
2022-10-04 23:35:26 +0330

    ♻️ refactor:Implemented singout function.

--- src/components/profile/Profile.js
+++ src/components/profile/Profile.js
@@ -5,11 +5,12 @@ import { useNavigate } from "react-router";
 import useToken from "../../hooks/useToken";
 import useUser from "../../hooks/useUser";
 import Card from "../card/Card";
+import { toast, ToastContainer } from "react-toastify";
 
 const Profile = function () {
   const [token, setToken] = useToken();
   const user = useUser();
-  const { id, email, info } = user;
+  const { id, email, isVerified, info } = user;
   const [errorMessage, setErrorMessage] = useState("");
   const [firstNameValue, setFirstNameValue] = useState(info.firstName || "");
   const [lastNameValue, setLastNameValue] = useState(info.lastName || "");
@@ -30,77 +31,84 @@ const Profile = function () {
       );
       const { token: newToken } = response.data;
       setToken(newToken);
-      console.log("true");
+      toast.success("Profile saved successfully!!!", {
+        position: toast.POSITION.TOP_CENTER,
+      });
     } catch (err) {
-      console.log(err);
+      toast.error(`${err.message}`, {
+        position: toast.POSITION.TOP_CENTER,
+      });
     }
   };
 
   const signOut = function () {
-    console.log("signout");
+    localStorage.removeItem("token");
+    navigate("/signin");
   };
   return (
-    <Grid
-      item
-      sx={{ display: "flex", justifyContent: "center", alignItems: "center" }}
-    >
-      <Card title={`Update Profile of ${email}`}>
-        <Grid item className="mb">
-          <TextField
-            fullWidth
-            onChange={(e) => setFirstNameValue(e.target.value)}
-            value={firstNameValue}
-            label="First Name"
-            type="text"
-          />
-        </Grid>
-        <Grid item className="mb">
-          <TextField
-            fullWidth
-            onChange={(e) => setLastNameValue(e.target.value)}
-            value={lastNameValue}
-            label="Last Name"
-            type="text"
-            variant="outlined"
-          />
-        </Grid>
+    <>
+      <Grid
+        item
+        sx={{ display: "flex", justifyContent: "center", alignItems: "center" }}
+      >
+        <Card title={`Update Profile of ${email}`}>
+          <Grid item className="mb">
+            <TextField
+              fullWidth
+              onChange={(e) => setFirstNameValue(e.target.value)}
+              value={firstNameValue}
+              label="First Name"
+              type="text"
+            />
+          </Grid>
+          <Grid item className="mb">
+            <TextField
+              fullWidth
+              onChange={(e) => setLastNameValue(e.target.value)}
+              value={lastNameValue}
+              label="Last Name"
+              type="text"
+              variant="outlined"
+            />
+          </Grid>
 
-        <Grid item className="mb">
-          <TextField
-            fullWidth
-            onChange={(e) => setBioValue(e.target.value)}
-            value={bioValue}
-            label="Last Name"
-            type="text"
-            variant="outlined"
-            multiline
-            maxRows={4}
-          />
-        </Grid>
+          <Grid item className="mb">
+            <TextField
+              fullWidth
+              onChange={(e) => setBioValue(e.target.value)}
+              value={bioValue}
+              label="Last Name"
+              type="text"
+              variant="outlined"
+              multiline
+              maxRows={4}
+            />
+          </Grid>
 
-        <Grid item className="mb">
-          <Button
-            fullWidth
-            onClick={() => saveChangesHandler()}
-            size="large"
-            variant="contained"
-          >
-            Save Changes
-          </Button>
-        </Grid>
-        <Grid item className="mb">
-          <Button
-            size="large"
-            variant="outlined"
-            fullWidth
-            color="error"
-            onClick={() => navigate("/signout")}
-          >
-            Sign Out
-          </Button>
-        </Grid>
-      </Card>
-    </Grid>
+          <Grid item className="mb">
+            <Button
+              fullWidth
+              onClick={() => saveChangesHandler()}
+              size="large"
+              variant="contained"
+            >
+              Save Changes
+            </Button>
+          </Grid>
+          <Grid item className="mb">
+            <Button
+              size="large"
+              variant="outlined"
+              fullWidth
+              color="error"
+              onClick={() => signOut()}
+            >
+              Sign Out
+            </Button>
+          </Grid>
+        </Card>
+      </Grid>
+    </>
   );
 };
 
