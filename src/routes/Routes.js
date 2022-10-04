b73cbcc8b40fbcb36578e41bf27c5e09e737ec23
farhdibehnamdev <97886945+farhdibehnamdev@users.noreply.github.com>
2022-10-04 23:39:07 +0330

    Added verify eamil route.

--- src/routes/Routes.js
+++ src/routes/Routes.js
@@ -1,22 +1,22 @@
-import {
-  BrowserRouter as Router,
-  Route,
-  Routes,
-  useNavigate,
-} from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
-import Profile from "../components/profile/Profile";
 import SignIn from "../components/signin/SignIn";
 import SignUp from "../components/signup/SignUp";
 import NotFound from "../components/notFound/NotFound";
+import PrivateRoute from "./privateRoute/PrivateRoute";
+import ProfilePage from "../pages/ProfilePage";
+import VerifyEmailPage from "../pages/VerifyEmailPage";
 
 const AppRoutes = function () {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Profile />} />
+        <Route path="/" element={<PrivateRoute />}>
+          <Route path="/profile" element={<ProfilePage />} />
+        </Route>
         <Route path="/signin" element={<SignIn />} />
         <Route path="/signup" element={<SignUp />} />
+        <Route path="/verify-email" element={<VerifyEmailPage />} />
         <Route path="/*" element={<NotFound />} />
       </Routes>
     </Router>
