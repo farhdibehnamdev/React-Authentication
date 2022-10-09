c2b8c4d58ded4a1718aba83c3cec4f30f8d8545c
farhdibehnamdev <97886945+farhdibehnamdev@users.noreply.github.com>
2022-10-09 19:41:44 +0330

    ✨ feat:Added password reset fail page.

---
+++ src/pages/PasswordResetFailPage.js
@@ -0,0 +1,7 @@
+import PasswordResetFail from "../components/passwordResetFail/PasswordResetFail";
+
+const PasswordResetFailPage = function () {
+  return <PasswordResetFail />;
+};
+
+export default PasswordResetFailPage;
